6b8d46d938cbb621e514a10c461bda70eeae1aeb
Monoke Kumu <monoke.kumu@illation.co.za>
2021-07-27 05:15:16 +0200

    askhrfrontend

--- src/App.js
+++ src/App.js
@@ -1,51 +1,52 @@
-//import logo from './logo.svg';
-//import xlsx from 'xlsx';
-//import * as XLSX from './js/xlsx';
 import React, {useState} from "react";
 import readXlsxFile from 'read-excel-file'
 
 import './App.css';
 
-var file;
-var arAskhrData = [];
-
 function App() {
-  const [data,setData] = useState(null);
+  var file;
+  const [data, setData] = useState(null);
+  const [type, settype] = useState(null);
+  const [loader, setloader] = useState(null);
+  let cntRun = 0;
 
-  function getxlxs(e){
+  function getxlxs(e) {
     file = e.target.files; // FileList object
+    settype("");
   }
-  
+
   function createArrayTicketId(files) {
-  
+    var arAskhrData = [];
+    setloader("load");
     function handleFileSelect() {
-      
+
       try {
         readXlsxFile(files[0]).then((rows) => {
           // `rows` is an array of rows    // each row being an array of cells.
           var xlxsData = rows;
           var xlxsheader = xlxsData[0];
-  
+
           if (xlxsheader != null && (xlxsheader[0] === "First Name" && xlxsheader[1] === "Last Name" && xlxsheader[2] === "Known As" &&
             xlxsheader[3] === "Facility" && xlxsheader[4] === "Employee number" && xlxsheader[5] === "Mobile Phone No" &&
             xlxsheader[6] === "Consent")) {
-  
+
             var name = "";
             var nickname = "";
             var Facility = "";
             var EmployeeNo = "";
             var phone = "";
             var Consent = "";
-  
+            var arLimite = [];
+            var totalItem = 0;
             for (var u = 1; u < xlxsData.length; u++) {
-  
+
               name = xlxsData[u][0] + " " + xlxsData[u][1];
               nickname = xlxsData[u][2];
               Facility = xlxsData[u][3];
               EmployeeNo = xlxsData[u][4];
               phone = xlxsData[u][5];
               Consent = xlxsData[u][6];
-  
+
               var askhr_fields = ({
                 "name": name,
                 "nickname": nickname,
@@ -54,67 +55,93 @@ function App() {
                 "phoneNo": phone,
                 "consent": Consent
               });
-  
-              arAskhrData.push(askhr_fields);
+              if (askhr_fields.empNum) {
+                arLimite.push(askhr_fields);
+                totalItem++;
+              }
+
+              if (arLimite.length === 50) {
+                arAskhrData.push(arLimite);
+                arLimite = [];
+              }
+
             }
-  
-  
             console.log('====================================');
-            saveData(arAskhrData);
+            if (arAskhrData.length > 0) {
+              arAskhrData.forEach((val) => {
+                val.forEach((value) => {
+                  saveData(value, totalItem);
+                });
+              });
+            }
             console.log('====================================');
-  
-  
+            if (arLimite.length > 0) {
+              arLimite.forEach((value) => {
+                saveData(value, totalItem);
+              });
+            }
+            console.log('====================================');
+
           } else {
             setData("The Excel file format given is not recognized. {Missing required Header}");
-  
+            settype("error");
+            setloader("");
           }
-          
+
         })
       } catch (error) {
         console.log(error);
+        setData("Something went wrong while extracting the file.");
+        settype("error");
+        setloader("");
       }
     }
-  
+
     handleFileSelect();
   }
-  
-  function saveData(data) {
-  
-    if (data.length > 0) {
+
+  function saveData(data, totalItem) {
+
+    if (data) {
       var myHeaders = new Headers();
       myHeaders.append("Content-Type", "application/json");
-      var url = "https://ra0353ccb7.execute-api.us-east-1.amazonaws.com/dev/askhr/add_many/employee";
-  
+      var url = "https://ra0353ccb7.execute-api.us-east-1.amazonaws.com/dev/askhr/add/employee";
+
       var raw = JSON.stringify(data);
-  
+
       var requestOptions = {
         method: 'POST',
         headers: myHeaders,
         body: raw,
         redirect: 'follow'
       };
-  
+
       fetch(url, requestOptions)
         .then(response => response.text())
         .then(result => {
-          var error_details = result["error_details"];
-          var count_added = result["count_added"];
-  
-          if (error_details.length <= 0) {
-            setData("The number of records created successfully is " + count_added);
-  
-          } else {
-            setData("The number of records created successfully is " + count_added + ", and the number of records failed is " + error_details.length);
+
+          cntRun++;
+
+          if (totalItem === cntRun) {
+            setData("The number of records created successfully is " + cntRun);
+            settype("success");
+            setloader("");
           }
-          
-          console.log(result)
+
         })
-        .catch(error => console.log('error', error));
-  
-  } else {
-    setData("We could not find records in the given file.");
-  } 
-  
+        .catch(error => {
+          console.log('error', error);
+          setData("Something went wrong while inserting the data.");
+          settype("error");
+          setloader("");
+        });
+
+    } else {
+      setData("We could not find records in the given file.");
+      settype("error");
+      setloader("");
+    }
+
   }
 
 
@@ -123,11 +150,11 @@ function App() {
       <header className="App-header">
         <div className="continer-fluid text-center mt-5">
           <div className="col-xs-12 col-md-12">
-            <h1 className="site-title">AskHr App</h1>
+            <h1 className="site-title">AskHr App</h1>            
             <div id="Loader">
-            {(data)?<div class="alert alert-warning mx-auto w-50" role="alert">{data}</div>:""}
+              {(loader === "load")?<div class="spinner-border text-primary spinner-border-sm" role="status"></div>:""}
+              {(data)?(type === "error")?<div class="alert alert-warning mx-auto w-75" role="alert">{data}</div>:(type === "success")?<div class="alert alert-success mx-auto w-50" role="alert">{data}</div>:"":""}
             </div>
-            {/*button id="export" class="btn btn-primary border-white" disabled><img src="images/Export_CSV.png" width="18" height="18" />Export as CSV</button*/}
             <div id="feedback-msg" className="mx-auto w-50" />
           </div>
           <div className="row text-center">
@@ -164,7 +191,5 @@ function App() {
     </div>
   );
 }
-
-
 
 export default App;
