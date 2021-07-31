f7ed8da073273a4392e92e1eda513a2b0be34757
Monoke Kumu <monoke.kumu@illation.co.za>
2021-07-31 08:01:49 +0200

    askhrfrontend

--- src/App.js
+++ src/App.js
@@ -13,7 +13,8 @@ function App() {
   let cntRun = 0;
 
   function getxlxs(e) {
-    file = e.target.files; // FileList object
+    file = e.target.files; 
+    //FileList object
     //console.log(file);
     settype("");
   }
@@ -151,44 +152,135 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className="continer-fluid text-center mb-5 w-50">
+        <div className="continer-fluid text-center mb-2 w-50">
           <div className="col-xs-12 col-md-12">
-          <img src={logo} className="App-logo" alt="logo" width="" height="" />
-            <h1 className="site-title">AskHr App</h1>            
+            <img
+              src={logo}
+              className="App-logo"
+              alt="logo"
+              width=""
+              height=""
+            />
+            <h1 className="site-title">AskHR Contact Information Portal</h1>
             <div id="Loader">
-              {(loader === "load")?<div class="spinner-border text-primary spinner-border-sm" role="status"></div>:""}
-              {(data)?(type === "error")?<div class="alert alert-warning mx-auto w-75" role="alert">{data}</div>:(type === "success")?<div class="alert alert-success mx-auto w-50" role="alert">{data}</div>:"":""}
+              {loader === "load" ? (
+                <div
+                className="spinner-border text-primary spinner-border-sm"
+                  role="status"
+                ></div>
+              ) : (
+                ""
+              )}
+              {data ? (
+                type === "error" ? (
+                  <div className="alert alert-warning mx-auto w-75" role="alert">
+                    {data}
+                  </div>
+                ) : type === "success" ? (
+                  <div className="alert alert-success mx-auto w-50" role="alert">
+                    {data}
+                  </div>
+                ) : (
+                  ""
+                )
+              ) : (
+                ""
+              )}
             </div>
             <div id="feedback-msg" className="mx-auto w-50" />
           </div>
-          <div className="row text-center mb-5">
-            <div className="mx-auto w-100 card pb-5 mt-5 bg-dark shadow shadow-lg">
+          <div className="row text-center mb-2">
+            <div className="mx-auto w-100 card pb-5 bg-transparent shadow shadow-lg">
               <div className="col-12 text-center">
-                
                 <center>
                   <div className="form-group col-8 mt-5">
-                   
                     <div className="mb-3 mt-2 w-auto">
                       <div className="input-group ">
-                        <label className="label mx-2 form-label" style={{ fontWeight: 'bold', color: 'azure' }}>Upload CSV:</label>
-                        <input type="file" accept='.xlsx, .xls' name="askhrCSV" onChange={getxlxs} className="form-control border rounded" id="askhrCSV" />
+                        <label
+                          className="label mx-2 form-label"
+                          style={{ fontWeight: "bold", color: "azure" }}
+                        >
+                          Upload CSV:
+                        </label>
+                        <input
+                          type="file"
+                          accept=".xlsx, .xls"
+                          name="askhrCSV"
+                          onChange={getxlxs}
+                          className="form-control border rounded"
+                          id="askhrCSV"
+                        />
                       </div>
                       <div className="askhrCSV-error text-danger" />
                     </div>
-                    <button onClick={()=>(file)?createArrayTicketId(file):console.log("no File selected"+file)} className=" btn btn-primary float-end border rounded btngetaskhr" id="btngetaskhr">
-                      Run Process
+                    <button
+                      onClick={() =>
+                        file
+                          ? createArrayTicketId(file)
+                          : console.log("no File selected" + file)
+                      }
+                      className=" btn float-end border rounded btngetaskhr"
+                      id="btngetaskhr"
+                      style={{backgroundColor:"#647687", color:"white"}}
+                    >
+                      Import Uploaded Files
                     </button>
                   </div>
                 </center>
               </div>
             </div>
           </div>
-          
         </div>
-        <div className="footer mt-5" style={{ marginTop: "200px", fontSize: 18 }}>
+        <div className="card accordion-item" style={{backgroundColor:"#647687"}}>
+          <div className="card-header accordion-header" id="headingOne">
+            <h5 className="mb-0 ">
+              <button
+                className="btn accordion-button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapseOne"
+                aria-expanded="true"
+                aria-controls="collapseOne"
+              >
+               To Import Files (.csv) format, please follow the steps.
+              </button>
+            </h5>
+          </div>
+
+          <div
+            id="collapseOne"
+            className="accordion-collapse collapse"
+            aria-labelledby="headingOne"
+            data-parent="#accordion"
+          >
+            <div className="card-body accordion-body">
+              <div className="form-inline form-control mt-1" id="Selected-HCP">
+                <ol className="list-styled">
+                  <li>Click on choose file button.</li>
+                  <li>Locate the file from the file location.</li>
+                  <li>Click on open.</li>
+                  <li>The file should appear in the box with the file name.</li>
+                  <li>Click on the "Import Uploaded File" button.</li>
+                  <li>You will receive a message indicating the result state of the process.</li>
+                </ol>
+
+              </div>
+            </div>
+          </div>
+        </div>
+        <div
+          className="footer mt-3"
+          style={{ marginTop: "200px", fontSize: 18 }}
+        >
           <p>
-            Copyright ©
-            All rights reserved | This template is made <i className="icon-heart" aria-hidden="true" /> by <a href="https://illation.co.za" rel="noreferrer" style={{ color: '#718199' }} target="_blank">illation</a>
+            Copyright © All rights reserved | Application Develop by{" "}
+            <a
+              href="https://illation.co.za"
+              rel="noreferrer"
+              style={{ color: "#718199" }}
+              target="_blank"
+            >
+              illation
+            </a>
           </p>
         </div>
       </header>
